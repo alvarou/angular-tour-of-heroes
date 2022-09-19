a5ecae41994c3b95572113a7951d957b31da7267
AU <urrutia.alvaro@gmail.com>
2022-09-19 13:11:52 -0500

    Update hero by Id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,28 +14,41 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
+  
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
 
-    /** GET heroes from the server */
-    getHeroes(): Observable<Hero[]> {
-      return this.http.get<Hero[]>(this.heroesUrl)
-        .pipe(
-          tap(_ => this.log('fetched heroes')),
-          catchError(this.handleError<Hero[]>('getHeroes', []))
-        );
-    }
-
-    /** GET hero by id. Will 404 if id not found */
-    getHero(id: number): Observable<Hero> {
-      const url = `${this.heroesUrl}/${id}`;
-      return this.http.get<Hero>(url).pipe(
-        tap(_ => this.log(`fetched hero id=${id}`)),
-        catchError(this.handleError<Hero>(`getHero id=${id}`))
+  /** GET heroes from the server */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
       );
-    }
+  }
+
+
+  /** GET hero by id. Will 404 if id not found */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
 
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
@@ -49,17 +62,17 @@ export class HeroService {
  * @param operation - name of the operation that failed
  * @param result - optional value to return as the observable result
  */
-private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
 
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
 
-    // TODO: better job of transforming error for user consumption
-    this.log(`${operation} failed: ${error.message}`);
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
 
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
 }
 }
